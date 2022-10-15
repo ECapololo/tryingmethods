3a1894fd5a6fae74bad439eef7247879f21f6ac2
Elisa Capololo <elisacapololo@meltwater.org>
2022-10-15 16:12:26 +0100

    It return a string of the day of the week based on the argument that was provided

--- TryingMethods.java
+++ TryingMethods.java
@@ -1,5 +1,37 @@
+//This class is for try work with statics and dinamics methods
+
 public class TryingMethods {
     public static void main(String[] args) {
-        
+        dayOfTheWeekString(6);
+    }
+
+    
+    public static String dayOfTheWeekString(int day) {
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                break;
+        }
+        return "";
     }
 }
