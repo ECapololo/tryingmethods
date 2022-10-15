a885500642f557fa42dd6260215f8cea68106d2f
Elisa Capololo <elisacapololo@meltwater.org>
2022-10-15 20:12:04 +0100

    second method

--- TryingMethods.java
+++ TryingMethods.java
@@ -2,6 +2,19 @@ public class TryingMethods {
     public static void main(String[] args) {
         TryingMethods test = new TryingMethods();
         test.dayOfTheWeekString(3);
+        test.message(12);
+    }
+
+    String message (int age){
+       if (age <= 17) {
+            System.out.println("Is a small children");
+        } else if (age >= 18 || age <= 60) {
+            System.out.println("Is a Young boy or girl");
+        } else {
+            System.out.println("Is a Older man");
+        }         
+        
+        return "";
     }
 
     String dayOfTheWeekString(int day) {
