3a856aad5df37e8b7b1b16a0d38539493db6bf95
Elisa Capololo <elisacapololo@meltwater.org>
2022-10-15 21:11:33 +0100

    sum all numbers between 12 and 103

--- TryingMethods.java
+++ TryingMethods.java
@@ -1,60 +1,14 @@
 public class TryingMethods {
     public static void main(String[] args) {
-        TryingMethods test = new TryingMethods();
-        test.dayOfTheWeekString(3);
-        test.message(12);
-        Add();
+        ForLoop();
     }
 
-    public static void Add() {
-        int number = 1;
+    static void ForLoop(){
         int sum = 0;
-        while (number < 1000) {
-            number += 1;
-            sum += number;
+        for (int i = 12; i <= 103; i++) {
+            sum += i;
         }
         System.out.println(sum);
-    }
-
-    String message (int age){
-       if (age <= 17) {
-            System.out.println("Is a small children");
-        } else if (age >= 18 || age <= 60) {
-            System.out.println("Is a Young boy or girl");
-        } else {
-            System.out.println("Is a Older man");
-        }         
-        
-        return "";
-    }
-
-    String dayOfTheWeekString(int day) {
-        switch (day) {
-            case 1:
-                System.out.println("Monday");
-                break;
-            case 2:
-                System.out.println("Tuesday");
-                break;
-            case 3:
-                System.out.println("Wednesday");
-                break;
-            case 4:
-                System.out.println("Thrusday");
-                break;
-            case 5:
-                System.out.println("Friday");
-                break;
-            case 6:
-                System.out.println("Saturday");
-                break;
-            case 7:
-                System.out.println("Sunday");
-                break;
-            default:
-                break;
-        }
-        return "";
     }
 
 }
