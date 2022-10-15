231d56f1a61954fd34a1d2cdeae7b7317dd48109
Elisa Capololo <elisacapololo@meltwater.org>
2022-10-15 20:58:17 +0100

    sum all numbers less than 1000

--- TryingMethods.java
+++ TryingMethods.java
@@ -1,49 +1,17 @@
 public class TryingMethods {
     public static void main(String[] args) {
-        TryingMethods test = new TryingMethods();
-        test.dayOfTheWeekString(3);
-        test.message(12);
+
+        Add();
     }
 
-    String message (int age){
-       if (age <= 17) {
-            System.out.println("Is a small children");
-        } else if (age >= 18 || age <= 60) {
-            System.out.println("Is a Young boy or girl");
-        } else {
-            System.out.println("Is a Older man");
-        }         
-        
-        return "";
-    }
-
-    String dayOfTheWeekString(int day) {
-        switch (day) {
-            case 1:
-                System.out.println("Monday");
-                break;
-            case 2:
-                System.out.println("Tuesday");
-                break;
-            case 3:
-                System.out.println("Wednesday");
-                break;
-            case 4:
-                System.out.println("Thrusday");
-                break;
-            case 5:
-                System.out.println("Friday");
-                break;
-            case 6:
-                System.out.println("Saturday");
-                break;
-            case 7:
-                System.out.println("Sunday");
-                break;
-            default:
-                break;
+    public static void Add() {
+        int number = 1;
+        int sum = 0;
+        while (number < 1000) {
+            number += 1;
+            sum += number;
         }
-        return "";
+        System.out.println(sum);
     }
 
 }
